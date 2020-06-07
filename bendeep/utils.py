1ed196455533e23bd5ae30c14d7c99fa45012da8
Sagor Sarker <sagorhem3532@gmail.com>
2020-06-07 11:27:08 +0600

    Update utils.py

--- bendeep/utils.py
+++ bendeep/utils.py
@@ -3,10 +3,10 @@ logger = logging.getLogger(__name__)
 
 try:
     import torch
-    _torch_available = True  # pylint: disable=invalid-name
+    _torch_available = True  
     logger.info("PyTorch version {} available.".format(torch.__version__))
 except ImportError:
-    _torch_available = False  # pylint: disable=invalid-name
+    _torch_available = False  
 
 def is_torch_available():
 	return _torch_available
